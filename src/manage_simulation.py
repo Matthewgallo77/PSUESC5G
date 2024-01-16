7db515540c2c54bacc2c9f2f6b7225cf87a8d081
Matthewgallo77 <mbg5733@psu.edu>
2024-01-16 14:54:03 -0500

    updates to simulation

--- src/manage_simulation.py
+++ src/manage_simulation.py
@@ -8,7 +8,8 @@ class SimulationManager:
         self.hfss = hfss_connection
         self.setup = None # this is the simulation object
         self.simulation_properties = None # simulation properties
-        self.solution_data = None
+        self.solution_data = None # solution data class
+        self.plot = None # post processor class
 
     def initalize_simulation_setup(self):
         
@@ -20,6 +21,8 @@ class SimulationManager:
 
     def run_simulation(self):
         """Run the simulation."""
-        self.solution_data = self.hfss.post
+        self.plot = self.hfss.post 
+        self.solution_data = self.plot.get_solution_data()
         self.hfss.analyze()
+        self.solution_data.export_data_to_csv("C:/Capstone_Project/5g_simulation/PSUESC5G/test.csv")
         # info = self.hfss.get_report_data()
